14109a62dbfabc2f66564173635e9750103a3783
MIQDAD <miqdadraja562@gmail.com>
2025-06-29 16:15:50 +0500

    Add root route to Flask app to prevent 404 on '/'

--- flask-service/app.py
+++ flask-service/app.py
@@ -3,10 +3,14 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify(message="Welcome to the Flask Service!")
+
 @app.route('/greet', methods=['GET'])
 def greet():
     user = os.environ.get('USER_NAME', 'World')
-    return jsonify(message=f"Hello, {user} from Flask!")
+    return jsonify(message=f"Hello, {user} lets learn DevOps!")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
